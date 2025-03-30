5c2641c082456f08a2efe54769d37a3b6762f079
Stone Chou <fu.runshi@stonybrook.edu>
2025-03-30 11:30:21 -0400

    Create Reconstruction_Structure.py

---
+++ Reconstruction_Structure.py
@@ -0,0 +1,5 @@
+From printtracks in event read energy deposition, pdg code, x,y,z coordinate  
+add a "CheckOneNeutronEvent" to make sure there is one neutron coming out of first vertex, if not,  select the one with smallest pdg code  
+add a "neutron_direction" function in event, input the information in printtracks, print out the real nuetron direction, calculated neutron direction  
+and the cos angle between two angles.  
+
